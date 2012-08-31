2d2ad05f5d277a57b89c0d24c04e2153b36af947
root <root@ubuntu.(none)>
2012-08-31 11:47:43 -0700

    Inital Setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Db2::Application.config.secret_token = '126d96d00b824452990c78c12a7665939975a1340ce1682d47b4cc64d6df2df3871ff72d9a8ddf9c5eb6ecb2ef1537e6c8f7c8877b3786e5b19a873d4f7e803b'
